074548c15718ef1e627b490fdd5af332d5fdd9a1
badchoiice <72988568+badchoiice@users.noreply.github.com>
2020-11-20 16:44:50 +0200

    Add files via upload

---
+++ tasks/landing2/js/script.js
@@ -0,0 +1,6 @@
+let icon = document.getElementById('icon');
+let menu = document.getElementById('menu');
+icon.onclick = function(){
+	menu.classList.toggle('active');
+	icon.classList.toggle('active-icon');
+}
