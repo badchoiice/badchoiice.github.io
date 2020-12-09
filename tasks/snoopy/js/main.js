1ec49eaf80a194109105d90efe6f94867a688f61
badchoiice <72988568+badchoiice@users.noreply.github.com>
2020-12-09 16:57:16 +0200

    Add files via upload

---
+++ tasks/snoopy/js/main.js
@@ -0,0 +1,7 @@
+header_burger.onclick = function(){
+	menu_log_mobile.classList.toggle("active")
+}
+size_row.onclick = function(){
+	size_opened.classList.toggle("active2");
+	size_btn.classList.toggle("open");
+}
