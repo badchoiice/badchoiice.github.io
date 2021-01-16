d79d068a02486435b168fe65d2f8a0645c8271b4
badchoiice <72988568+badchoiice@users.noreply.github.com>
2021-01-16 21:47:19 +0200

    Add files via upload

--- tasks/daisy/js/main.js
+++ tasks/daisy/js/main.js
@@ -1,15 +1,3 @@
-function ibg(){
-	let ibg = document.querySelectorAll('.ibg');
-	let container =document.querySelectorAll('.container')
-	for(var i=0; i<container.length; i++){
-		if(ibg[i].querySelector('img')){
-			container[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
-			ibg[i].querySelector('img').remove()
-		}
-	};
-	console.log(container)
-}
-ibg()
 document.getElementById('burger_menu').onclick = function(){
 	document.getElementById('mobile_menu').classList.toggle('active')
 }
